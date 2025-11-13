d6e49ca3b064cd1402321f4cdad73e02cf537165
Logan Kairns <27wolvy@gmail.com>
2025-11-13 10:56:52 -0500

    Initial commit

---
+++ backend/src/main/java/com/example/aidigitalwardrobe/AiDigitalWardrobeApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidigitalwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDigitalWardrobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDigitalWardrobeApplication.class, args);
+    }
+
+}
